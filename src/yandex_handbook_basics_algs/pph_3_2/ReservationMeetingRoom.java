3c134af52f280ec696ba0b9e0f8ffa3bab28f728
Angelina <a.hurok@gmail.com>
2024-11-30 23:34:36 +0300

    added classes ReservationMeetingRoom and Time for the solution of task from paragraph 3.2

---
+++ src/yandex_handbook_basics_algs/pph_3_2/ReservationMeetingRoom.java
@@ -0,0 +1,10 @@
+package yandex_handbook_basics_algs.pph_3_2;
+
+import java.util.ArrayList;
+
+public class ReservationMeetingRoom {
+    public static int f1(int n, ArrayList<Time> clients)
+    {
+        return 0;
+    }
+}
