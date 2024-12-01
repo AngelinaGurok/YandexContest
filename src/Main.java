08623f63a9dfffe9725d1aa991c19cd435b38231
Angelina <a.hurok@gmail.com>
2024-12-02 02:43:34 +0300

    fixed solution

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import yandex_handbook_basics_algs.pph_3_2.Time;
 //import yandex_handbook_basics_algs.pph_3_1.Premutations;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -14,57 +15,47 @@ public class Main {
         ArrayList<Main.Time> clients = new ArrayList<>(amountOfClients);
         int beginning = 1;
         int end = 2;
-        /*for(int i = 0; i < amountOfClients; i++){
+        for(int i = 0; i < amountOfClients; i++){
             beginning = in.nextInt();
             end = in.nextInt();
             Time time = new Time(beginning, end);
             clients.add(time);
-        }*/
+        }
         for(int i = 0; i < amountOfClients; i++){
-            Time time = new Time(beginning, end);
-            clients.add(time);
-            beginning += 1;
-            end += 1;
+            System.out.println(clients.get(i).getEnd());
+        }
+        Collections.sort(clients);
+
+        for(int i = 0; i < amountOfClients; i++){
+            System.out.println(clients.get(i).getEnd());
         }
         int result = Main.countAmountOfClients(0, clients);
         System.out.println(result);
     }
 
-    static int findMinimumEnd(int index, ArrayList<Main.Time> clients){
-        int minEnd = clients.get(index).getEnd();
-        int indexOfMin = index;
-        for(int i = 1; i < clients.size(); i++){
-            if(minEnd > clients.get(i).getEnd()){
-                indexOfMin = i;
-                minEnd = clients.get(i).getEnd();
-            }
-        }
-        return indexOfMin;
-    }
     public static int countAmountOfClients(int index, ArrayList<Main.Time> clients)
     {
         int result = clients.size();
         if(result <= index) return result;
-        int indexOfMinEnd = Main.findMinimumEnd(index, clients);
 
-        int minBeginning = clients.get(indexOfMinEnd).getBeginning();
-        int minEnd = clients.get(indexOfMinEnd).getEnd();
+        int minBeginning = clients.get(index).getBeginning();
+        int minEnd = clients.get(index).getEnd();
 
         for(int i = index; i < clients.size(); i++){
-            if(i != indexOfMinEnd){
+            if(i != index){
                 int currentBeginning = clients.get(i).getBeginning();
                 int currentEnd = clients.get(i).getEnd();
 
                 if(((currentBeginning >= minBeginning) && (currentBeginning <= minEnd)) ||
-                        ((currentEnd >= minBeginning) && (currentEnd <= minEnd))){
+                        ((currentEnd >= minBeginning) && (currentEnd <= minEnd)) || (currentBeginning < minBeginning && currentEnd > minEnd)){
                     clients.remove(i);
                     i--;
                 }
             }
         }
-        return countAmountOfClients(indexOfMinEnd + 1, clients);
+        return countAmountOfClients(index + 1, clients);
     }
-    public static class Time {
+    public static class Time implements Comparable<Main.Time> {
         int beginning;
         int end;
         public Time(int beginning, int end){
@@ -78,6 +69,11 @@ public class Main {
 
         public int getEnd() {
             return end;
+        }
+
+        @Override
+        public int compareTo(Time o) {
+                return this.getEnd() - o.getEnd();
         }
     }
 }
