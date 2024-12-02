14f171ae0b40f55faf764af1de12034bbaf8a59d
Angelina <a.hurok@gmail.com>
2024-12-02 23:41:29 +0300

    started task from paragraph 3.3

---
+++ src/yandex_handbook_basics_algs/pph_3_3/Rocks.java
@@ -0,0 +1,12 @@
+package yandex_handbook_basics_algs.pph_3_3;
+
+public class Rocks {
+    int n;
+    int m;
+    public Rocks(int n, int m){
+        this.n = n;
+        this.m = m;
+    }
+
+    
+}
